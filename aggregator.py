49aab9196ac14ed0835eb626fcf41d7efe421ec0
volbil <volbilnexus@gmail.com>
2023-04-25 22:51:33 +0300

    Improve companies import speed

--- aggregator.py
+++ aggregator.py
@@ -7,5 +7,5 @@ import asyncio
 if __name__ == "__main__":
     # asyncio.run(aggregator_anime_genres())
     # asyncio.run(aggregator_characters())
-    # asyncio.run(aggregator_companies())
-    asyncio.run(aggregator_people())
+    asyncio.run(aggregator_companies())
+    # asyncio.run(aggregator_people())
--- service/sync/aggregator/companies.py
+++ service/sync/aggregator/companies.py
@@ -2,60 +2,78 @@ from service.models import Company
 from tortoise import Tortoise
 from service import utils
 from . import requests
+import asyncio
 import config
+
+async def make_request(semaphore, page):
+    async with semaphore:
+        data = await requests.get_companies(page)
+        return data["list"]
+    
+async def save_companies(data):
+    references = [entry["reference"] for entry in data]
+
+    cache = await Company.filter(content_id__in=references)
+
+    companies_cache = {entry.content_id: entry for entry in cache}
+
+    create_companies = []
+    update_companies = []
+
+    for company_data in data:
+        updated = utils.from_timestamp(company_data["updated"])
+        slug = utils.slugify(
+            company_data["name"], company_data["reference"]
+        )
+
+        if company_data["reference"] in companies_cache:
+            company = companies_cache[company_data["reference"]]
+
+            if company.updated == updated:
+                continue
+
+            if company.favorites == company_data["favorites"]:
+                continue
+
+            company.favorites = company_data["favorites"]
+            company.updated = updated
+
+            update_companies.append(company)
+
+            print(f"Updated company: {company.name} ({company.favorites})")
+
+        else:
+            company = Company(**{
+                "content_id": company_data["reference"],
+                "favorites": company_data["favorites"],
+                "name": company_data["name"],
+                "updated": updated,
+                "slug": slug
+            })
+
+            create_companies.append(company)
+
+            print(f"Added company: {company.name} ({company.favorites})")
+
+    await Company.bulk_create(create_companies)
+
+    if len(update_companies) > 0:
+        await Company.bulk_update(update_companies, fields=[
+            "updated", "favorites"
+        ])
 
 async def aggregator_companies():
     await Tortoise.init(config=config.tortoise)
     await Tortoise.generate_schemas()
 
-    pages = 1
-    page = 1
+    data = await requests.get_companies(1)
+    pages = data["pagination"]["pages"]
 
-    while page <= pages:
-        data = await requests.get_companies(page)
-        pages = data["pagination"]["pages"]
+    semaphore = asyncio.Semaphore(20)
+    tasks = [make_request(semaphore, page) for page in range(1, pages + 1)]
 
-        create_companies = []
-        update_companies = []
+    result = await asyncio.gather(*tasks)
 
-        for company_data in data["list"]:
-            updated = utils.from_timestamp(company_data["updated"])
-            slug = utils.slugify(
-                company_data["name"], company_data["reference"]
-            )
+    data = [item for sublist in result for item in sublist]
 
-            if company := await Company.filter(slug=slug).first():
-                if company.updated == updated:
-                    continue
-
-                if company.favorites == company_data["favorites"]:
-                    continue
-
-                company.favorites = company_data["favorites"]
-                company.updated = updated
-
-                update_companies.append(company)
-
-                print(f"Updated company: {company.name} ({company.favorites})")
-
-            else:
-                company = Company(**{
-                    "content_id": company_data["reference"],
-                    "favorites": company_data["favorites"],
-                    "name": company_data["name"],
-                    "updated": updated,
-                    "slug": slug
-                })
-
-                create_companies.append(company)
-
-                print(f"Added company: {company.name} ({company.favorites})")
-
-        await Company.bulk_create(create_companies)
-
-        if len(update_companies) > 0:
-            await Company.bulk_update(update_companies, fields=[
-                "updated", "favorites"
-            ])
-
-        page += 1
+    await save_companies(data)
