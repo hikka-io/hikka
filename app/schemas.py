e750d5394c790257bebbc6efa3a6c00c5c67acf2
volbil <volbilnexus@gmail.com>
2023-06-08 03:45:42 +0300

    Added examples to common schemas

--- app/schemas.py
+++ app/schemas.py
@@ -24,81 +24,87 @@ class ORJSONModel(BaseModel):
 
 # Enums
 class WatchStatusEnum(str, Enum):
-    planned = constants.WATCH_PLANNED
-    watching = constants.WATCH_WATCHING
     completed = constants.WATCH_COMPLETED
+    watching = constants.WATCH_WATCHING
+    planned = constants.WATCH_PLANNED
     on_hold = constants.WATCH_ON_HOLD
     dropped = constants.WATCH_DROPPED
 
 
 # Args
 class PaginationArgs(ORJSONModel):
-    page: int = Field(default=1, gt=0)
+    page: int = Field(default=1, gt=0, example=1)
 
 
 # Responses
 class PaginationResponse(ORJSONModel):
-    total: int
-    pages: int
-    page: int
+    total: int = Field(example=20)
+    pages: int = Field(example=2)
+    page: int = Field(example=1)
 
 
 class QuerySearchArgs(ORJSONModel):
     query: Union[constr(min_length=3, max_length=255), None] = None
-    page: int = Field(default=1, gt=0)
+    page: int = Field(default=1, gt=0, example=1)
 
 
 class AnimeResponse(ORJSONModel):
-    media_type: Union[str, None]
-    title_ua: Union[str, None]
-    title_en: Union[str, None]
-    title_ja: Union[str, None]
-    episodes: Union[int, None]
-    poster: Union[str, None]
-    status: Union[str, None]
-    scored_by: int
-    score: float
-    slug: str
+    media_type: Union[str, None] = Field(example="tv")
+    title_ua: Union[str, None] = Field(
+        example="Цей прекрасний світ, благословенний Богом!"
+    )
+    title_en: Union[str, None] = Field(
+        example="KonoSuba: God's Blessing on This Wonderful World!"
+    )
+    title_ja: Union[str, None] = Field(
+        example="Kono Subarashii Sekai ni Shukufuku wo!"
+    )
+    episodes: Union[int, None] = Field(example="10")
+    poster: Union[str, None] = Field(example="https://cdn.hikka.io/hikka.jpg")
+    status: Union[str, None] = Field(example="finished")
+    scored_by: int = Field(example=1210150)
+    score: float = Field(example=8.11)
+    slug: str = Field(example="kono-subarashii-sekai-ni-shukufuku-wo-123456")
 
 
 class CharacterResponse(ORJSONModel):
-    name_ua: Union[str, None]
-    name_en: Union[str, None]
-    name_ja: Union[str, None]
-    image: Union[str, None]
-    slug: str
+    name_ua: Union[str, None] = Field(example="Меґумін")
+    name_en: Union[str, None] = Field(example="Megumin")
+    name_ja: Union[str, None] = Field(example="めぐみん")
+    image: Union[str, None] = Field(example="https://cdn.hikka.io/hikka.jpg")
+    slug: str = Field(example="megumin-123456")
 
 
 class PersonResponse(ORJSONModel):
-    name_native: Union[str, None]
-    name_ua: Union[str, None]
-    name_en: Union[str, None]
-    image: Union[str, None]
-    slug: str
+    name_native: Union[str, None] = Field(example="高橋 李依")
+    name_ua: Union[str, None] = Field(example="Ріє Такахаші")
+    name_en: Union[str, None] = Field(example="Rie Takahashi")
+    image: Union[str, None] = Field(example="https://cdn.hikka.io/hikka.jpg")
+    slug: str = Field(example="rie-takahashi-123456")
 
 
 class AnimeFavouriteResponse(ORJSONModel):
+    reference: str = Field(example="c773d0bf-1c42-4c18-aec8-1bdd8cb0a434")
+    created: datetime = Field(example=1686088809)
     anime: AnimeResponse
-    created: datetime
-    reference: str
 
 
 class WatchResponse(ORJSONModel):
-    note: Union[str, None]
+    reference: str = Field(example="c773d0bf-1c42-4c18-aec8-1bdd8cb0a434")
+    updated: datetime = Field(example=1686088809)
+    created: datetime = Field(example=1686088809)
+    note: Union[str, None] = Field(example="🤯")
+    status: str = Field(example="watching")
+    episodes: int = Field(example=3)
+    score: int = Field(example=8)
     anime: AnimeResponse
-    updated: datetime
-    created: datetime
-    reference: str
-    episodes: int
-    status: str
-    score: int
 
 
 class SuccessResponse(ORJSONModel):
-    success: bool
+    success: bool = Field(example=True)
 
 
 class CompanyResponse(ORJSONModel):
-    image: Union[str, None]
-    name: str
-    slug: str
+    image: Union[str, None] = Field(example="https://cdn.hikka.io/hikka.jpg")
+    slug: str = Field(example="hikka-inc-123456")
+    name: str = Field(example="Hikka Inc.")
