b75721133dd3b0987107f0e85834e8254aadabe6
volbil <volbilnexus@gmail.com>
2023-08-01 21:38:16 +0300

    Extra signup test and bug fixes

--- tests/test_auth_signup.py
+++ tests/test_auth_signup.py
@@ -33,3 +33,33 @@ async def test_signup(client, test_session):
     # Login
     response = await request_login(client, "user@mail.com", "password")
     assert response.status_code == status.HTTP_200_OK
+
+    # Login with bad password
+    response = await request_login(client, "user@mail.com", "bad_password")
+    assert response.json()["code"] == "auth_invalid_password"
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+async def test_signup_duplicate(client, test_session):
+    # Create new account
+    response = await request_signup(
+        client, "user@mail.com", "username", "password"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    # Create new account with duplicate email
+    response = await request_signup(
+        client, "user@mail.com", "username2", "password"
+    )
+
+    assert response.json()["code"] == "auth_email_exists"
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    # Create new account with duplicate username
+    response = await request_signup(
+        client, "user2@mail.com", "username", "password"
+    )
+
+    assert response.json()["code"] == "auth_username_taken"
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
