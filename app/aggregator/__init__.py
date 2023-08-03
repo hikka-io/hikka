a8f8bd7dad0c2384578dd7a1f972cba3e0b3158a
volbil <volbilnexus@gmail.com>
2023-08-03 22:02:25 +0300

    Decoupled anime info sync logic

--- app/aggregator/__init__.py
+++ app/aggregator/__init__.py
@@ -2,4 +2,5 @@ from .franchises import save_anime_franchises_list
 from .characters import save_characters
 from .companies import save_companies
 from .genres import save_anime_genres
+from .info import update_anime_info
 from .anime import save_anime_list
