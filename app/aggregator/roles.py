8002d2cf03470782cf0d3e7928fbbdb52da53bcc
volbil <volbilnexus@gmail.com>
2023-08-03 22:23:26 +0300

    Decoupled people and roles sync logic

---
+++ app/aggregator/roles.py
@@ -0,0 +1,27 @@
+from app.models import AnimeStaffRole
+from sqlalchemy import select
+from app import utils
+
+
+async def update_anime_roles(session, data):
+    create_roles = []
+
+    for role_name in data:
+        slug = utils.slugify(role_name)
+
+        if await session.scalar(select(AnimeStaffRole).filter_by(slug=slug)):
+            continue
+
+        role = AnimeStaffRole(
+            **{
+                "name_en": role_name,
+                "slug": slug,
+            }
+        )
+
+        create_roles.append(role)
+
+        print(f"Added anime staff role: {role.name_en}")
+
+    session.add_all(create_roles)
+    await session.commit()
--- app/sync/aggregator/people.py
+++ app/sync/aggregator/people.py
@@ -1,7 +1,5 @@
 from app.database import sessionmanager
-from app.models import Person, Image
-from sqlalchemy import select
-from datetime import datetime
+from app import aggregator
 from . import requests
 from app import utils
 import asyncio
@@ -15,77 +13,7 @@ async def make_request(semaphore, page):
 
 async def save_people(data):
     async with sessionmanager.session() as session:
-        content_ids = [entry["content_id"] for entry in data]
-        images = [entry["image"] for entry in data]
-
-        cache = await session.scalars(
-            select(Person).where(Person.content_id.in_(content_ids))
-        )
-
-        people_cache = {entry.content_id: entry for entry in cache}
-
-        cache = await session.scalars(
-            select(Image).where(Image.path.in_(images))
-        )
-
-        image_cache = {entry.path: entry for entry in cache}
-
-        add_people = []
-
-        for person_data in data:
-            updated = utils.from_timestamp(person_data["updated"])
-            slug = utils.slugify(
-                person_data["name_en"], person_data["content_id"]
-            )
-
-            if person_data["content_id"] in people_cache:
-                person = people_cache[person_data["content_id"]]
-
-                if person.updated == updated:
-                    continue
-
-                if person.favorites == person_data["favorites"]:
-                    continue
-
-                person.favorites = person_data["favorites"]
-                person.updated = updated
-
-                add_people.append(person)
-
-                print(f"Updated person: {person.name_en} ({person.favorites})")
-
-            else:
-                if not (image := image_cache.get(person_data["image"])):
-                    if person_data["image"]:
-                        image = Image(
-                            **{
-                                "path": person_data["image"],
-                                "created": datetime.utcnow(),
-                                "uploaded": True,
-                                "ignore": False,
-                            }
-                        )
-
-                        image_cache[person_data["image"]] = image
-
-                person = Person(
-                    **{
-                        "content_id": person_data["content_id"],
-                        "name_native": person_data["name_ja"],
-                        "favorites": person_data["favorites"],
-                        "name_en": person_data["name_en"],
-                        "image_relation": image,
-                        "updated": updated,
-                        "slug": slug,
-                    }
-                )
-
-                add_people.append(person)
-
-                print(f"Added person: {person.name_en} ({person.favorites})")
-
-        session.add_all(add_people)
-        await session.commit()
+        await aggregator.save_people(session, data)
 
 
 async def aggregator_people():
