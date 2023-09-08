56d4196079808c4dc6157a8aeb76b826a35b2358
volbil <volbilnexus@gmail.com>
2023-09-09 00:07:29 +0300

    Tests for anime produced by company

--- tests/client_requests.py
+++ tests/client_requests.py
@@ -159,5 +159,10 @@ def request_companies_info(client, slug):
     return client.get(f"/companies/{slug}")
 
 
-def request_companies_anime(client, slug):
-    return client.get(f"/companies/{slug}/anime")
+def request_companies_anime(client, slug, company_type=None):
+    endpoint = f"/companies/{slug}/anime"
+
+    if company_type:
+        endpoint += f"?type={company_type}"
+
+    return client.get(endpoint)
--- tests/companies/test_companies_anime.py
+++ tests/companies/test_companies_anime.py
@@ -8,24 +8,55 @@ async def test_companies_anime(
     aggregator_anime,
     aggregator_anime_info,
 ):
-    # Get anime produced by MAPPA
-    response = await request_companies_anime(client, "mappa-360033")
+    # Get anime produced by Bones
+    response = await request_companies_anime(client, "bones-b0b61b")
 
     assert response.status_code == status.HTTP_200_OK
-
-    from pprint import pprint
-
-    pprint(response.json())
 
     # Check slugs
     assert response.json()["list"][0]["type"] == "studio"
     assert (
         response.json()["list"][0]["anime"]["slug"]
-        == "shingeki-no-kyojin-the-final-season-kanketsu-hen-833be1"
+        == "fullmetal-alchemist-brotherhood-fc524a"
     )
 
-    assert response.json()["list"][2]["type"] == "studio"
+    assert response.json()["list"][1]["type"] == "producer"
     assert (
-        response.json()["list"][2]["anime"]["slug"]
-        == "shingeki-no-kyojin-the-final-season-part-2-8d3fc3"
+        response.json()["list"][1]["anime"]["slug"]
+        == "pia-carrot-e-youkoso-sayaka-no-koi-monogatari-227414"
+    )
+
+
+async def test_companies_anime_filter(
+    client,
+    aggregator_companies,
+    aggregator_anime,
+    aggregator_anime_info,
+):
+    # Get anime produced by Bones (studio)
+    response = await request_companies_anime(client, "bones-b0b61b", "studio")
+
+    assert response.status_code == status.HTTP_200_OK
+
+    # Check data
+    assert len(response.json()["list"]) == 1
+
+    assert response.json()["list"][0]["type"] == "studio"
+    assert (
+        response.json()["list"][0]["anime"]["slug"]
+        == "fullmetal-alchemist-brotherhood-fc524a"
+    )
+
+    # Get anime produced by Bones (producer)
+    response = await request_companies_anime(client, "bones-b0b61b", "producer")
+
+    assert response.status_code == status.HTTP_200_OK
+
+    # Check data
+    assert len(response.json()["list"]) == 1
+
+    assert response.json()["list"][0]["type"] == "producer"
+    assert (
+        response.json()["list"][0]["anime"]["slug"]
+        == "pia-carrot-e-youkoso-sayaka-no-koi-monogatari-227414"
     )
