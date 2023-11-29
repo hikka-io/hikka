86350005d54d2f3a34d3856d8ed254644a95718f
volbil <volbilnexus@gmail.com>
2023-11-29 02:54:35 +0200

    One big ugly tests for watch list ordering

--- tests/watch/test_watch_list.py
+++ tests/watch/test_watch_list.py
@@ -53,12 +53,12 @@ async def test_watch_list(
 
     assert response.status_code == status.HTTP_200_OK
     assert len(response.json()["list"]) == 2
-    assert response.json()["list"][0]["status"] == "planned"
-    assert response.json()["list"][1]["status"] == "watching"
-    assert response.json()["list"][0]["anime"]["slug"] == "oshi-no-ko-421060"
+    assert response.json()["list"][0]["status"] == "watching"
+    assert response.json()["list"][1]["status"] == "planned"
     assert (
-        response.json()["list"][1]["anime"]["slug"] == "bocchi-the-rock-9e172d"
+        response.json()["list"][0]["anime"]["slug"] == "bocchi-the-rock-9e172d"
     )
+    assert response.json()["list"][1]["anime"]["slug"] == "oshi-no-ko-421060"
 
     # Try filtering by watch entry staus
     response = await request_watch_list(client, "testuser", "watching")
@@ -79,3 +79,143 @@ async def test_watch_list(
     assert len(response.json()["list"]) == 1
     assert response.json()["list"][0]["status"] == "planned"
     assert response.json()["list"][0]["anime"]["slug"] == "oshi-no-ko-421060"
+
+
+async def test_watch_list_order(
+    client,
+    create_test_user,
+    aggregator_anime,
+    get_test_token,
+):
+    # ToDo: this test ideally should be split into bunch of smaller tests
+
+    # Add Bocchi to watch list
+    await request_watch_add(
+        client,
+        "bocchi-the-rock-9e172d",
+        get_test_token,
+        {
+            "status": "watching",
+            "episodes": 1,
+            "score": 8,
+        },
+    )
+
+    # Add one more anime to watch list
+    await request_watch_add(
+        client,
+        "kimi-no-na-wa-945779",
+        get_test_token,
+        {
+            "status": "planned",
+            "episodes": 0,
+            "score": 0,
+        },
+    )
+
+    # And one more just in case
+    await request_watch_add(
+        client,
+        "kaguya-sama-wa-kokurasetai-ultra-romantic-fcd761",
+        get_test_token,
+        {
+            "status": "completed",
+            "episodes": 13,
+            "score": 9,
+        },
+    )
+
+    # Check score asc order
+    response = await request_watch_list(
+        client, "testuser", order="score", sort="asc"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["list"][0]["score"] == 0
+    assert response.json()["list"][1]["score"] == 8
+    assert response.json()["list"][2]["score"] == 9
+
+    assert response.json()["list"][0]["anime"]["slug"] == "kimi-no-na-wa-945779"
+    assert (
+        response.json()["list"][1]["anime"]["slug"] == "bocchi-the-rock-9e172d"
+    )
+    assert (
+        response.json()["list"][2]["anime"]["slug"]
+        == "kaguya-sama-wa-kokurasetai-ultra-romantic-fcd761"
+    )
+
+    # Check episodes desc order
+    response = await request_watch_list(
+        client, "testuser", order="episodes", sort="desc"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["list"][0]["episodes"] == 13
+    assert response.json()["list"][1]["episodes"] == 1
+    assert response.json()["list"][2]["episodes"] == 0
+
+    assert (
+        response.json()["list"][0]["anime"]["slug"]
+        == "kaguya-sama-wa-kokurasetai-ultra-romantic-fcd761"
+    )
+    assert (
+        response.json()["list"][1]["anime"]["slug"] == "bocchi-the-rock-9e172d"
+    )
+    assert response.json()["list"][2]["anime"]["slug"] == "kimi-no-na-wa-945779"
+
+    # Check episodes asc order
+    response = await request_watch_list(
+        client, "testuser", order="episodes", sort="asc"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["list"][0]["episodes"] == 0
+    assert response.json()["list"][1]["episodes"] == 1
+    assert response.json()["list"][2]["episodes"] == 13
+
+    assert response.json()["list"][0]["anime"]["slug"] == "kimi-no-na-wa-945779"
+    assert (
+        response.json()["list"][1]["anime"]["slug"] == "bocchi-the-rock-9e172d"
+    )
+    assert (
+        response.json()["list"][2]["anime"]["slug"]
+        == "kaguya-sama-wa-kokurasetai-ultra-romantic-fcd761"
+    )
+
+    # Check media type desc order
+    response = await request_watch_list(
+        client, "testuser", order="media_type", sort="desc"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["list"][0]["anime"]["media_type"] == "tv"
+    assert response.json()["list"][1]["anime"]["media_type"] == "tv"
+    assert response.json()["list"][2]["anime"]["media_type"] == "movie"
+
+    assert (
+        response.json()["list"][0]["anime"]["slug"]
+        == "kaguya-sama-wa-kokurasetai-ultra-romantic-fcd761"
+    )
+    assert (
+        response.json()["list"][1]["anime"]["slug"] == "bocchi-the-rock-9e172d"
+    )
+    assert response.json()["list"][2]["anime"]["slug"] == "kimi-no-na-wa-945779"
+
+    # Check media type asc order
+    response = await request_watch_list(
+        client, "testuser", order="media_type", sort="asc"
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["list"][0]["anime"]["media_type"] == "movie"
+    assert response.json()["list"][1]["anime"]["media_type"] == "tv"
+    assert response.json()["list"][2]["anime"]["media_type"] == "tv"
+
+    assert response.json()["list"][0]["anime"]["slug"] == "kimi-no-na-wa-945779"
+    assert (
+        response.json()["list"][1]["anime"]["slug"]
+        == "kaguya-sama-wa-kokurasetai-ultra-romantic-fcd761"
+    )
+    assert (
+        response.json()["list"][2]["anime"]["slug"] == "bocchi-the-rock-9e172d"
+    )
