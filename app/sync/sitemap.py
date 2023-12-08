d327c8c32e1d2f8558c920b6b93c79a4f38baa11
volbil <volbilnexus@gmail.com>
2023-12-08 23:40:51 +0200

    Sitemap

---
+++ app/sync/sitemap.py
@@ -0,0 +1,57 @@
+from app.database import sessionmanager
+from app.utils import get_settings
+from app.utils import to_timestamp
+from sqlalchemy import desc, func
+from sqlalchemy import select
+from app.models import Anime
+import json
+import math
+
+
+async def generate_sitemap(session):
+    result = []
+
+    limit = 10000
+    total = await session.scalar(select(func.count(Anime.id)))
+    pages = math.ceil(total / limit) + 1
+
+    for page in range(1, pages):
+        offset = (limit * (page)) - limit
+
+        data = await session.execute(
+            select(Anime.slug, Anime.updated)
+            .order_by(
+                desc(Anime.score),
+                desc(Anime.scored_by),
+                desc(Anime.id),
+            )
+            .limit(limit)
+            .offset(offset)
+        )
+
+        for entry in data:
+            result.append(
+                {
+                    "updated_at": to_timestamp(entry[1]),
+                    "slug": entry[0],
+                }
+            )
+
+    return result
+
+
+async def update_sitemap():
+    """Generate sitemap file"""
+
+    settings = get_settings()
+
+    sessionmanager.init(settings.database.endpoint)
+
+    async with sessionmanager.session() as session:
+        with open(
+            f"{settings.backend.sitemap_path}/sitemap_anime.json", "w"
+        ) as file:
+            result = await generate_sitemap(session)
+            file.write(json.dumps(result))
+
+    await sessionmanager.close()
--- sync.py
+++ sync.py
@@ -5,9 +5,11 @@ import asyncio
 def init_scheduler():
     scheduler = AsyncIOScheduler()
 
+    from app.sync import update_sitemap
     from app.sync import send_emails
 
     scheduler.add_job(send_emails, "interval", seconds=10)
+    scheduler.add_job(update_sitemap, "interval", days=1)
     scheduler.start()
 
     try:
