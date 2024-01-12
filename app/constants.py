75a02c0cacaea4823a7bda3a116aa51d798c25c4
volbil <volbilnexus@gmail.com>
2024-01-12 19:16:21 +0200

    Moved aggregator endpoint to settings

--- app/constants.py
+++ app/constants.py
@@ -2,9 +2,6 @@
 EMAIL_ACTIVATION = "activation"
 EMAIL_PASSWORD_RESET = "password_reset"
 
-# Aggregator endpoint
-AGGREGATOR = "http://localhost:7777/database"
-
 # CDN endpoing
 CDN_ENDPOINT = "https://cdn.hikka.io"
 
--- app/sync/aggregator/requests.py
+++ app/sync/aggregator/requests.py
@@ -1,9 +1,12 @@
+from app.utils import get_settings
 from app import constants
 import aiohttp
 
 
 async def get_anime_genres():
-    endpoint = f"{constants.AGGREGATOR}/genres/anime"
+    settings = get_settings()
+
+    endpoint = f"{settings.backend.aggregator}/genres/anime"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as r:
@@ -12,7 +15,9 @@ async def get_anime_genres():
 
 
 async def get_anime_roles():
-    endpoint = f"{constants.AGGREGATOR}/roles/anime"
+    settings = get_settings()
+
+    endpoint = f"{settings.backend.aggregator}/roles/anime"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as r:
@@ -21,7 +26,9 @@ async def get_anime_roles():
 
 
 async def get_companies(page):
-    endpoint = f"{constants.AGGREGATOR}/companies?page={page}"
+    settings = get_settings()
+
+    endpoint = f"{settings.backend.aggregator}/companies?page={page}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as r:
@@ -30,7 +37,9 @@ async def get_companies(page):
 
 
 async def get_characters(page):
-    endpoint = f"{constants.AGGREGATOR}/characters?page={page}"
+    settings = get_settings()
+
+    endpoint = f"{settings.backend.aggregator}/characters?page={page}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as r:
@@ -39,7 +48,9 @@ async def get_characters(page):
 
 
 async def get_people(page):
-    endpoint = f"{constants.AGGREGATOR}/people?page={page}"
+    settings = get_settings()
+
+    endpoint = f"{settings.backend.aggregator}/people?page={page}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as r:
@@ -48,7 +59,9 @@ async def get_people(page):
 
 
 async def get_anime(page):
-    endpoint = f"{constants.AGGREGATOR}/anime?page={page}"
+    settings = get_settings()
+
+    endpoint = f"{settings.backend.aggregator}/anime?page={page}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as r:
@@ -57,7 +70,9 @@ async def get_anime(page):
 
 
 async def get_anime_franchises(page):
-    endpoint = f"{constants.AGGREGATOR}/franchises/anime?page={page}"
+    settings = get_settings()
+
+    endpoint = f"{settings.backend.aggregator}/franchises/anime?page={page}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as r:
@@ -66,7 +81,9 @@ async def get_anime_franchises(page):
 
 
 async def get_anime_info(content_id):
-    endpoint = f"{constants.AGGREGATOR}/anime/{content_id}"
+    settings = get_settings()
+
+    endpoint = f"{settings.backend.aggregator}/anime/{content_id}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as r:
