4013464e95b0f096f3c2532f69105f101d81aaa9
volbil <volbilnexus@gmail.com>
2024-01-22 23:45:50 +0200

    Added cover image to user

--- app/models/user/user.py
+++ app/models/user/user.py
@@ -79,13 +79,19 @@ class User(Base):
         back_populates="moderator",
     )
 
+    avatar_image_relation: Mapped["Image"] = relationship(lazy="selectin")
     avatar_image_id = mapped_column(
         ForeignKey("service_images.id", ondelete="SET NULL"),
         nullable=True,
         index=True,
     )
 
-    avatar_image_relation: Mapped["Image"] = relationship(lazy="selectin")
+    cover_image_relation: Mapped["Image"] = relationship(lazy="selectin")
+    cover_image_id = mapped_column(
+        ForeignKey("service_images.id", ondelete="SET NULL"),
+        nullable=True,
+        index=True,
+    )
 
     @hybrid_property
     def avatar(self):
@@ -99,6 +105,19 @@ class User(Base):
             return "https://cdn.hikka.io/avatar.jpg"
 
         return self.avatar_image_relation.url
+
+    @hybrid_property
+    def cover(self):
+        if not self.cover_image_relation:
+            return None
+
+        if (
+            self.cover_image_relation.ignore
+            or not self.cover_image_relation.uploaded
+        ):
+            return None
+
+        return self.cover_image_relation.url
 
     @hybrid_property
     def active(self):
