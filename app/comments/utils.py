73659cccb31a341a58ec7d3a1ae660dd414e1cad
volbil <volbilnexus@gmail.com>
2024-01-27 20:05:42 +0200

    Don't allow empty markdown

--- app/comments/utils.py
+++ app/comments/utils.py
@@ -1,5 +1,6 @@
 from .schemas import CommentNode
 from datetime import timedelta
+import re
 
 
 # Convert uuid reference to comment path
@@ -73,3 +74,18 @@ def round_hour(date):
         seconds=date.second,
         microseconds=date.microsecond,
     )
+
+
+def is_empty_markdown(text):
+    # First we remove markdown tags
+    text = re.sub(r"\*\*(.*?)\*\*", r"\1", text)  # **text**
+    text = re.sub(r"\*(.*?)\*", r"\1", text)  # *text*
+    text = re.sub(r"__(.*?)__", r"\1", text)  # __text__
+    text = re.sub(r"\[.*?\]\((.*?)\)", r"\1", text)  # [text](link)
+    text = re.sub(r":::spoiler (.*?):::", r"\1", text)  # :::spoiler content :::
+
+    # Then remove spaces
+    text = text.replace(" ", "")
+
+    # And now check if string is empty
+    return len(text) == 0
