4c3ced77ef831fbb3de6c97e9ed59bd78d739136
volbil <volbilnexus@gmail.com>
2024-01-28 21:59:30 +0200

    Addeded comment vote model

---
+++ app/models/comments/vote.py
@@ -0,0 +1,18 @@
+from ..mixins import CreatedMixin, UpdatedMixin
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+from ..base import Base
+
+
+class CommentVote(Base, CreatedMixin, UpdatedMixin):
+    __tablename__ = "service_comment_votes"
+
+    score: Mapped[int]  # Shoule be -1 / 0 / 1
+
+    user_id = mapped_column(ForeignKey("service_users.id"))
+    user: Mapped["User"] = relationship(foreign_keys=[user_id])
+
+    comment_id = mapped_column(ForeignKey("service_comments.id"))
+    comment: Mapped["Comment"] = relationship(foreign_keys=[comment_id])
