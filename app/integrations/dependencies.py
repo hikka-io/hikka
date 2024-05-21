c8bc63ba3db535768ce7e209325edbef6ded6fd0
volbil <volbilnexus@gmail.com>
2024-05-22 01:08:10 +0300

    RIP Watari

--- app/integrations/dependencies.py
+++ app/integrations/dependencies.py
@@ -3,17 +3,7 @@ from app.database import get_session
 from app.models import Anime
 from app.errors import Abort
 from fastapi import Depends
-from uuid import UUID
 from . import service
-
-
-async def validate_watari_anime(
-    slug: UUID, session: AsyncSession = Depends(get_session)
-) -> Anime:
-    if not (anime := await service.get_anime_by_watari(session, slug)):
-        raise Abort("anime", "not-found")
-
-    return anime
 
 
 async def validate_mal_anime(
--- app/integrations/router.py
+++ app/integrations/router.py
@@ -1,75 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from .dependencies import validate_mal_anime
 from fastapi import APIRouter, Depends
+from app.schemas import AnimeResponse
 from app.database import get_session
 from .schemas import MALAnimeArgs
 from app.models import Anime
 from . import service
 
-from .dependencies import (
-    validate_watari_anime,
-    validate_mal_anime,
-)
-
-from app.schemas import (
-    AnimeExternalResponse,
-    AnimeVideoResponse,
-    AnimeStaffResponse,
-    CharacterResponse,
-    AnimeResponse,
-)
-
 
 router = APIRouter(prefix="/integrations", tags=["Integrations"])
-
-
-@router.get("/watari/{slug}/characters", response_model=list[CharacterResponse])
-async def watari_characters(
-    anime: Anime = Depends(validate_watari_anime),
-    session: AsyncSession = Depends(get_session),
-):
-    return await service.get_main_characters(session, anime)
-
-
-@router.get("/watari/{slug}/related", response_model=list[str])
-async def watari_related(
-    anime: Anime = Depends(validate_watari_anime),
-    session: AsyncSession = Depends(get_session),
-):
-    related = await service.get_watari_related(session, anime)
-
-    return [
-        url["url"][-36:]
-        for external in related
-        for url in external
-        if url["text"] == "Watari Anime"
-    ]
-
-
-@router.get("/watari/{slug}/staff", response_model=list[AnimeStaffResponse])
-async def watari_staff(
-    anime: Anime = Depends(validate_watari_anime),
-    session: AsyncSession = Depends(get_session),
-):
-    staff = await service.get_anime_main_staff(session, anime)
-    return staff.unique().all()
-
-
-@router.get("/watari/{slug}/videos", response_model=list[AnimeVideoResponse])
-async def watari_videos(anime: Anime = Depends(validate_watari_anime)):
-    return anime.videos[:8]
-
-
-@router.get(
-    "/watari/{slug}/external",
-    response_model=list[AnimeExternalResponse],
-)
-async def watari_external(anime: Anime = Depends(validate_watari_anime)):
-    return anime.external[:8]
-
-
-@router.get("/watari/{slug}", response_model=AnimeResponse)
-async def watari_anime(anime: Anime = Depends(validate_watari_anime)):
-    return anime
 
 
 @router.get("/mal/anime/{mal_id}", response_model=AnimeResponse)
