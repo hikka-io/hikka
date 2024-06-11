75507271d71321aaaa1a1a4a5cd8947c2372b881
volbil <volbilnexus@gmail.com>
2024-06-12 01:09:24 +0300

    Handle aiohttp error properly

--- app/system/router.py
+++ app/system/router.py
@@ -11,30 +11,32 @@ router = APIRouter(include_in_schema=False)
 async def analytics_event(request: Request, args: EventArgs):
     settings = get_settings()
 
-    # TODO: handle failed aiohttp correctly
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                f"{settings.backend.plausible}/api/event",
+                data={
+                    "referrer": args.r,
+                    "domain": args.d,
+                    "name": args.n,
+                    "url": args.u,
+                },
+                headers={
+                    "User-Agent": request.headers.get("user-agent", "unknown"),
+                    "Host": "analytics.hikka.io",
+                    "X-Forwarded-Host": request.headers.get(
+                        "x-forwarded-host", "hikka.io"
+                    ),
+                    "X-Forwarded-Proto": request.headers.get(
+                        "x-forwarded-proto", "https"
+                    ),
+                    "X-Forwarded-For": request.headers.get(
+                        "cf-connecting-ip",
+                        request.headers.get("x-forwarded-for", "127.0.0.1"),
+                    ),
+                },
+            ):
+                return {}
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            f"{settings.backend.plausible}/api/event",
-            data={
-                "referrer": args.r,
-                "domain": args.d,
-                "name": args.n,
-                "url": args.u,
-            },
-            headers={
-                "User-Agent": request.headers.get("user-agent", "unknown"),
-                "Host": "analytics.hikka.io",
-                "X-Forwarded-Host": request.headers.get(
-                    "x-forwarded-host", "hikka.io"
-                ),
-                "X-Forwarded-Proto": request.headers.get(
-                    "x-forwarded-proto", "https"
-                ),
-                "X-Forwarded-For": request.headers.get(
-                    "cf-connecting-ip",
-                    request.headers.get("x-forwarded-for", "127.0.0.1"),
-                ),
-            },
-        ):
-            return {}
+    except aiohttp.ClientError:
+        return {}
