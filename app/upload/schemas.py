4ba2992092c4829ef416b970055923e3bc81fb9d
volbil <volbilnexus@gmail.com>
2024-12-23 00:41:46 +0200

    Attachment upload

--- app/upload/schemas.py
+++ app/upload/schemas.py
@@ -6,7 +6,7 @@ from enum import Enum
 
 # Enums
 class UploadTypeEnum(str, Enum):
-    article_cover = constants.UPLOAD_ARTICLE_COVER
+    attachment = constants.UPLOAD_ATTACHMENT
     avatar = constants.UPLOAD_AVATAR
     cover = constants.UPLOAD_COVER
 
