2f517e37c604fcd6b990d8bcbdfb38148f094aea
volbil <volbilnexus@gmail.com>
2024-12-26 14:01:57 +0200

    Update article by moderator

--- tests/articles/test_articles_update.py
+++ tests/articles/test_articles_update.py
@@ -74,3 +74,56 @@ async def test_articles_update(
     assert log.data["before"]["title"] != log.data["after"]["title"]
     assert log.data["before"]["draft"] != log.data["after"]["draft"]
     assert log.data["before"]["text"] != log.data["after"]["text"]
+
+
+async def test_articles_update_moderator(
+    client,
+    create_test_user_moderator,
+    create_dummy_user,
+    get_test_token,
+    get_dummy_token,
+    test_session,
+):
+    response = await request_create_article(
+        client,
+        get_dummy_token,
+        {
+            "cover": None,
+            "text": "Lorem ipsum dor sit amet.",
+            "title": "Interesting title",
+            "tags": ["interesting", "tag"],
+            "category": "news",
+            "content": None,
+            "draft": False,
+            "trusted": False,
+        },
+    )
+
+    # Make sure we got correct response code
+    assert response.status_code == status.HTTP_200_OK
+
+    article_slug = response.json()["slug"]
+
+    response = await request_update_article(
+        client,
+        article_slug,
+        get_test_token,
+        {
+            "cover": None,
+            "text": "Amet sit dor ipsum lorem.",
+            "title": "Amazing title",
+            "tags": ["wow", "tag"],
+            "category": "news",
+            "content": None,
+            "draft": True,
+            "trusted": False,
+        },
+    )
+
+    # Let's check status again
+    assert response.status_code == status.HTTP_200_OK
+
+    # Check log
+    log = await test_session.scalar(select(Log).order_by(desc(Log.created)))
+    assert log.log_type == constants.LOG_ARTICLE_UPDATE
+    assert log.user == create_test_user_moderator
