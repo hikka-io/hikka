c28a61eabbf744f50d6687b0138cca07717d8c4a
Yaroslaw Biloshytskyi <yaroslaw0upwork@gmail.com>
2025-06-29 12:22:42 +0300

    Add user_id indexes to user history and watch/read models

--- app/models/list/read.py
+++ app/models/list/read.py
@@ -23,7 +23,7 @@ class Read(Base, CreatedMixin, UpdatedMixin, DeletedMixin):
     content_type: Mapped[str]
     content_id: Mapped[UUID]
 
-    user_id = mapped_column(ForeignKey("service_users.id"))
+    user_id = mapped_column(ForeignKey("service_users.id"), index=True)
 
     user: Mapped["User"] = relationship(foreign_keys=[user_id])
 
