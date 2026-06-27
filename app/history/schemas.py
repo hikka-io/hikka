648ec415ef175c1faa2a258048bf26071199c64c
olexh <typicaladmsky@gmail.com>
2026-06-27 20:57:27 +0300

    Type notification_type and history_type response fields as enums

--- app/history/schemas.py
+++ app/history/schemas.py
@@ -1,4 +1,6 @@
 from app.schemas import datetime_pd
+from app import constants
+from enum import Enum
 
 from app.schemas import (
     PaginationResponse,
@@ -10,13 +12,31 @@ from app.schemas import (
 )
 
 
+# Enums
+class HistoryTypeEnum(str, Enum):
+    watch = constants.HISTORY_WATCH
+    watch_delete = constants.HISTORY_WATCH_DELETE
+    read_manga = constants.HISTORY_READ_MANGA
+    read_manga_delete = constants.HISTORY_READ_MANGA_DELETE
+    read_novel = constants.HISTORY_READ_NOVEL
+    read_novel_delete = constants.HISTORY_READ_NOVEL_DELETE
+    watch_import = constants.HISTORY_WATCH_IMPORT
+    read_import = constants.HISTORY_READ_IMPORT
+    favourite_anime_add = constants.HISTORY_FAVOURITE_ANIME
+    favourite_anime_remove = constants.HISTORY_FAVOURITE_ANIME_REMOVE
+    favourite_manga_add = constants.HISTORY_FAVOURITE_MANGA
+    favourite_manga_remove = constants.HISTORY_FAVOURITE_MANGA_REMOVE
+    favourite_novel_add = constants.HISTORY_FAVOURITE_NOVEL
+    favourite_novel_remove = constants.HISTORY_FAVOURITE_NOVEL_REMOVE
+
+
 # Responses
 class HistoryResponse(CustomModel):
     content: AnimeResponse | MangaResponse | NovelResponse | None = None
     created: datetime_pd
     updated: datetime_pd
     user: UserResponse
-    history_type: str
+    history_type: HistoryTypeEnum
     reference: str
     data: dict
 
--- app/notifications/schemas.py
+++ app/notifications/schemas.py
@@ -1,11 +1,32 @@
 from app.schemas import PaginationResponse, CustomModel, UserResponse
 from app.schemas import datetime_pd
+from app import constants
+from enum import Enum
+
+
+# Enums
+class NotificationTypeEnum(str, Enum):
+    comment_reply = constants.NOTIFICATION_COMMENT_REPLY
+    comment_vote = constants.NOTIFICATION_COMMENT_VOTE
+    comment_tag = constants.NOTIFICATION_COMMENT_TAG
+    collection_comment = constants.NOTIFICATION_COLLECTION_COMMENT
+    article_comment = constants.NOTIFICATION_ARTICLE_COMMENT
+    collection_vote = constants.NOTIFICATION_COLLECTION_VOTE
+    article_vote = constants.NOTIFICATION_ARTICLE_VOTE
+    edit_comment = constants.NOTIFICATION_EDIT_COMMENT
+    edit_accepted = constants.NOTIFICATION_EDIT_ACCEPTED
+    edit_denied = constants.NOTIFICATION_EDIT_DENIED
+    edit_updated = constants.NOTIFICATION_EDIT_UPDATED
+    hikka_update = constants.NOTIFICATION_HIKKA_UPDATE
+    schedule_anime = constants.NOTIFICATION_SCHEDULE_ANIME
+    follow = constants.NOTIFICATION_FOLLOW
+    thirdparty_login = constants.NOTIFICATION_THIRDPARTY_LOGIN
 
 
 # Responses
 class NotificationResponse(CustomModel):
     initiator_user: UserResponse | None
-    notification_type: str
+    notification_type: NotificationTypeEnum
     created: datetime_pd
     reference: str
     seen: bool
